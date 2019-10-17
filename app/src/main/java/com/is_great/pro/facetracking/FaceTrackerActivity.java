a6797a66beb410bb2032552c6f865f77ee7d63fd
Wilber Cutire <wilbert_030@icloud.com>
2019-10-17 07:23:30 -0500

    first commit

---
+++ app/src/main/java/com/is_great/pro/facetracking/FaceTrackerActivity.java
@@ -0,0 +1,298 @@
+package com.is_great.pro.facetracking;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.is_great.pro.facetracking.ui.camera.CameraSourcePreview;
+import com.is_great.pro.facetracking.ui.camera.GraphicOverlay;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+
+public final class FaceTrackerActivity extends AppCompatActivity {
+    private static final String TAG = "FaceTracker";
+
+    private CameraSource mCameraSource = null;
+
+    private CameraSourcePreview mPreview;
+    private GraphicOverlay mGraphicOverlay;
+    //private TextView mUpdates;
+
+    private static final int RC_HANDLE_GMS = 9001;
+
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+
+    private int token;
+
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+
+        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
+        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+        //mUpdates = (TextView) findViewById(R.id.faceUpdates);
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        } else {
+            requestCameraPermission();
+        }
+        final int min = 0;
+        final int max = 80000000;
+        final int random = new Random().nextInt((max - min) + 1) + min;
+        token = random;
+    }
+
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        Snackbar.make(mGraphicOverlay, R.string.permission_camera_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, listener)
+                .show();
+    }
+
+    private void createCameraSource() {
+
+        Context context = getApplicationContext();
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        detector.setProcessor(
+                new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory())
+                        .build());
+
+        if (!detector.isOperational()) {
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+        }
+
+        mCameraSource = new CameraSource.Builder(context, detector)
+                .setRequestedPreviewSize(1024, 720)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(30.0f)
+                .setAutoFocusEnabled(true)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCameraSource();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCameraSource != null) {
+            mCameraSource.release();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source");
+            // we have permission, so create the camerasource
+            createCameraSource();
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Face Tracker sample")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
+    }
+
+    private void startCameraSource() {
+
+        // check that the device has play services available.
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
+                getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dlg =
+                    GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dlg.show();
+        }
+
+        if (mCameraSource != null) {
+            try {
+                mPreview.start(mCameraSource, mGraphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                mCameraSource.release();
+                mCameraSource = null;
+            }
+        }
+    }
+
+    private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new GraphicFaceTracker(mGraphicOverlay,FaceTrackerActivity.this);
+        }
+    }
+
+    private class GraphicFaceTracker extends Tracker<Face> {
+        private GraphicOverlay mOverlay;
+        private FaceGraphic mFaceGraphic;
+
+        GraphicFaceTracker(GraphicOverlay overlay,Context context) {
+            mOverlay = overlay;
+            mFaceGraphic = new FaceGraphic(overlay,context);
+        }
+
+        @Override
+        public void onNewItem(int faceId, Face item) {
+            mFaceGraphic.setId(faceId);
+        }
+
+        @Override
+        public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
+            Bitmap image = loadBitmapFromView(mOverlay, mOverlay.getWidth(), mOverlay.getHeight());
+            float x = mFaceGraphic.translateX(face.getPosition().x + face.getWidth() / 2);
+            float y = mFaceGraphic.translateY(face.getPosition().y + face.getHeight() / 2);
+            float xOffset = mFaceGraphic.scaleX(face.getWidth() / 2.0f);
+            float yOffset = mFaceGraphic.scaleY(face.getHeight() / 2.0f);
+            int left = (int)(x - xOffset);
+            if (left < 0 ){
+                left = 0;
+            }
+            int top = (int) (y - yOffset);
+            if (top < 0) {
+                top = 0;
+            }
+            int right = (int) (x + xOffset);
+            if (right > image.getWidth()){
+                right = image.getWidth();
+            }
+            int bottom = (int) (y + yOffset);
+            if (bottom > image.getHeight()){
+                bottom = image.getHeight();
+            }
+            Bitmap croppedBitmap = Bitmap.createBitmap(image, left, top, right-left, bottom-top);
+            String path = Environment.getExternalStorageDirectory().toString();
+            if (!fileExists(getApplicationContext(), "face" + token + "-" + face.getId()+".png")) {
+                File file = new File("/data/user/0/com.is_great.pro.facetracking/files/face"+token+"-"+face.getId()+".png");
+                Log.d("FileName", file.getAbsolutePath() +" no existe");
+                try (FileOutputStream out = new FileOutputStream(file)) {
+                    croppedBitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                    // PNG is a lossless format, the compression factor (100) is ignored
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("FileName", face.getId()+".png" +" existe");
+            }
+            //Log.d("Face", left + " " + bottom + " " + right + " " + bottom + " " + image.getWidth() + " " + image.getHeight());
+            mOverlay.add(mFaceGraphic);
+            mFaceGraphic.updateFace(face);
+        }
+
+        @Override
+        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
+            mOverlay.remove(mFaceGraphic);
+        }
+
+        @Override
+        public void onDone() {
+            mOverlay.remove(mFaceGraphic);
+        }
+    }
+
+    public static Bitmap loadBitmapFromView(View v, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width , height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(0, 0, width, height);
+//Get the view’s background
+        Drawable bgDrawable =v.getBackground();
+        if (bgDrawable!=null)
+//has background drawable, then draw it on the canvas
+            bgDrawable.draw(c);
+        else
+//does not have background drawable, then draw white background on the canvas
+            c.drawColor(Color.WHITE);
+        v.draw(c);
+        return b;
+    }
+
+    public boolean fileExists(Context context, String filename) {
+        File file = context.getFileStreamPath(filename);
+        Log.d("FileName", file.getAbsolutePath());
+        if(file == null || !file.exists()) {
+            return false;
+        }
+        return true;
+    }
+}
